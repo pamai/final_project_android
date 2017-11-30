52f45e63d0537b033662c42ef1e2bd286c5b8f1e
pamai <pamai_5656@hotmail.com>
2017-11-30 22:58:28 +0700

    Add loading indicator

--- PocketSchedule/app/src/main/java/kmitl/paniti58070080/pocketschedule/LoginActivity.java
+++ PocketSchedule/app/src/main/java/kmitl/paniti58070080/pocketschedule/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
@@ -18,33 +19,43 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.wang.avi.AVLoadingIndicatorView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private CallbackManager callbackManager;
     private AccessToken accessToken;
     private FirebaseAuth mAuth;
+    private AVLoadingIndicatorView loading;
+    private View loginView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         mAuth = FirebaseAuth.getInstance();
         callbackManager = CallbackManager.Factory.create();
         LoginButton loginButton = findViewById(R.id.login_button);
         loginButton.setReadPermissions("email");
         accessToken = AccessToken.getCurrentAccessToken();
+
+        loading = findViewById(R.id.loading);
+        loginView = findViewById(R.id.loginView);
+        loading.hide();
         // Other app specific specialization
 
         // Callback registration
         if (accessToken != null){
+            loginView.setClickable(false);
+            loading.show();
             handleFacebookAccessToken(accessToken);
         }
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
+                loginView.setClickable(false);
+                loading.show();
                 handleFacebookAccessToken(loginResult.getAccessToken());
             }
 
@@ -73,10 +84,13 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+                            loginView.setClickable(true);
+                            loading.hide();
                             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                             startActivity(intent);
                             finish();
                         } else {
+                            loading.hide();
                             Toast.makeText(LoginActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
